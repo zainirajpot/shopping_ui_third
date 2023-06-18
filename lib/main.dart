aeeb5b061a308df2fa21cbf944a8e6ea25901c0d
Zainab Rajpot <zainichohan712@gmail.com>
2023-06-18 19:28:12 +0500

    Updated code

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Shopping Ui',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
